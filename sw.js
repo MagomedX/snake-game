a7559c168b8620ad1f0d8ee27a274465fb27dcc7
MagomedX <165846007+MagomedX@users.noreply.github.com>
2026-05-09 23:54:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
+self.addEventListener('install',e=>{
 
+e.waitUntil(
+
+caches.open('snake-cache').then(cache=>{
+
+return cache.addAll([
+'/',
+'/index.html',
+'/manifest.json',
+'/theme.mp3'
+]);
+
+})
+
+);
+
+});
+
+self.addEventListener('fetch',e=>{
+
+e.respondWith(
+
+caches.match(e.request).then(response=>{
+
+return response || fetch(e.request);
+
+})
+
+);
+
+});
